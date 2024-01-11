57b2e491bdf6116bfa0bd1a2adc3f60536d6c04c
ayşe <ayse.efsin@yandex.com>
2024-01-11 04:51:50 +0300

    project done

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,18 @@ public class Main {
         System.out.println("height= " + wall.getHeight());
 
         System.out.println("area= " + wall.getArea());
+        System.out.println("Age: " + person.getAge());
+
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 
 }
